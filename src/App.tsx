40dc401598d5b3e1315d45d86883a2ff1542f303
AfamaVal <progfams@gmail.com>
2021-06-05 12:30:26 +0100

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Card from "./components/Card";
+import React, { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Card from './components/Card'
+import Footer from './components/Footer'
 
 const App = () => {
   // const [age, setAge] = useState<number | string>("5");
 
   return (
-    <div className="container">
-      <Header />
-      <Card />
-    </div>
-  );
-};
+    <>
+      <div className='container'>
+        <Header />
+        <Card />
+      </div>
+      <Footer />
+    </>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = (): JSX.Element => {
+  return (
+    <div className='bg-dark text-white text-center py-4'>
+      &copy; Afams Val 2021
+    </div>
+  )
+}
+
+export default Footer
